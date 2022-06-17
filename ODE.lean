07db343792b18d0a4366fa822885d95d5260e946
Xubai Wang <self@xubai.wang>
2022-06-17 18:53:29 +0800

    feat: basic structure

---
+++ ODE.lean
@@ -0,0 +1,6 @@
+import ODE.Chap1
+import ODE.Chap2
+import ODE.Chap3
+import ODE.Chap4
+import ODE.Chap5
+import ODE.Chap6
--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,19 @@
 import Lake
 open Lake DSL
 
-package math2501 {
-  -- add package configuration options here
-}
+package math2501
 
+/--
+支持性定义，如实数、微积分等
+
+Supporting definitions like real number and calculus
+-/
+lean_lib Supporting
+
+/--
+按书本章节进行证明
+
+Proof accorded with the chapter order of the reference book
+-/
 @[defaultTarget]
-lean_lib Math2501 {
-  -- add library configuration options here
-}
+lean_lib ODE
